708a0641bb260032f0b481d3771de6b9215d3897
Tarun Gorre <gtarun16@gmail.com>
2019-06-02 18:01:50 +0200

    Added App icons

--- Movies AppTests/ViewControllerTests.swift
+++ Movies AppTests/ViewControllerTests.swift
@@ -56,12 +56,4 @@ class ViewControllerTests: XCTestCase {
         XCTAssertEqual(actualReuseIdentifer, expectedReuseIdentifier)
     }
 
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
